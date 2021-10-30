c43245ea0a27cf01f8be05867cc576dc070ca691
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-10-30 10:48:32 -0300

    setTimeOut

---
+++ 01_setTimeout.js
@@ -0,0 +1,15 @@
+// synchronous
+console.log("I ")
+console.log("eat ")
+console.log("ice cream ")
+console.log("with a ")
+console.log("spoon.")
+
+// asynchronous
+setTimeout(() => {
+  console.log("ice cream ")
+}, 4000)
+console.log("I ")
+console.log("eat ")
+console.log("with a ")
+console.log("spoon.")
